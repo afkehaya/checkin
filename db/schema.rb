3750b7f667676ca728a87ad03f7a9d6adf16b2af
Alexander Kehaya <afkehaya@gmail.com>
2014-12-21 08:48:53 -0700

    debuged contrller now working on db for views page

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141219153129) do
+ActiveRecord::Schema.define(version: 20141219162930) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "questions"
+    t.integer  "user_id"
   end
+
+  add_index "pins", ["user_id"], name: "index_pins_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
