3d27013dca2f0621e32c95cb13c985cc761a9e04
Alexander Kehaya <afkehaya@gmail.com>
2014-12-31 08:37:51 -0800

    added students to activeadmin

---
+++ app/admin/user.rb
@@ -0,0 +1,32 @@
+ActiveAdmin.register User do
+  menu false
+  ActiveAdmin.register User, as: "Student" do
+   
+
+   index do
+    column :name 
+    column "View Checkin", :id do |checkin|
+            link_to checkin.id, [:admin, checkin]
+          end
+    column "Email", :email
+    column "Last Signed In", :last_sign_in_at
+    column "Number of Sign Ins", :sign_in_count
+    
+  
+  end
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+
+  end 
+end
